49269f7906734187e064ad018eb868eef4bdf063
Nyrew <lukas.vokel@gmail.com>
2025-01-18 14:18:19 +0100

    changed main

--- test/test_render.py
+++ test/test_render.py
@@ -1,12 +1,14 @@
 import requests
 
-url = "https://butter-scraper.onrender.com/get_latest_data" # docker - render
+url = "https://butter-scraper.onrender.com/scrape_save" # docker - render
 
 try:
-    response = requests.get(url)
+    response = requests.post(url)
     response.raise_for_status()  # Vyvolá chybu, pokud HTTP status není 200
     data = response.json()
     print("Response from Render endpoint:")
     print(data)
+    for item in data:
+        print(item)
 except requests.exceptions.RequestException as e:
     print(f"Error connecting to endpoint: {e}")
