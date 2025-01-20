c20d960decb15771556c7c33031f97f550bbcca3
Nyrew <lukas.vokel@gmail.com>
2025-01-20 19:38:34 +0100

    changed names of the pics

--- test/test_render.py
+++ test/test_render.py
@@ -1,9 +1,9 @@
 import requests
 
-url = "https://butter-scraper.onrender.com/get_price_history/'Tatra Máslo'" # docker - render
+url = "https://butter-scraper.onrender.com/scrape" # docker - render
 
 try:
-    response = requests.get(url)
+    response = requests.post(url)
     response.raise_for_status()  # Vyvolá chybu, pokud HTTP status není 200
     data = response.json()
     print("Response from Render endpoint:")
